25ba576621d062f8f166dd74d560d7bd9f9228e0
nayyarv <nayyarv@gmail.com>
2019-05-20 20:47:46 +1000

    Updated pelican quickstart

--- publishconf.py
+++ publishconf.py
@@ -10,6 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
+# If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://nayyarv.github.io'
 RELATIVE_URLS = False
 
